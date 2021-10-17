3e543a2cc2afcbb47dabd0aa8c8366187333acba
Aritra Biswas <pandalearnstocode@gmail.com>
2021-10-17 07:36:19 +0530

    adding docstring."

--- minipackage/main.py
+++ minipackage/main.py
@@ -1,6 +1,15 @@
 def hello_world():
+    """This is a hello world function.
+
+    Returns:
+        str: Hello world
+    """
     return "Hello World!"
 
-
 def hello_mcu():
+    """This is a hello mcu function.
+
+    Returns:
+        str: hello mcu
+    """
     return "Hello MCU!"
